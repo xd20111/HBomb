b6206d5811c9fff33b3232873812bfefbf07115b
Honey Pots <54258853+HoneyPots0@users.noreply.github.com>
2020-04-05 16:01:15 +0530

    Add files via upload

--- HBomb.py
+++ HBomb.py
@@ -139,10 +139,10 @@ elif int(bomb) == 4 :
 
 elif int(bomb) == 5 :
         webbrowser.open('https://honey0pots.000webhostapp.com/p/HBomb', new=2)
-	print("If You Use Mobile . May be Website not open automatically \n Visit : https://honey0pots.000webhostapp.com/p/HBomb/")
-	input("Press Enter To Run HBomb Tool Again : ")
+        print("If You Use Mobile . May be Website not open automatically \n Visit : https://honey0pots.000webhostapp.com/p/HBomb/")
+        input("\nPress Enter To Run HBomb Tool Again : ")
         subprocess.call([sys.executable, 'HBomb.py'])
-        
+
 elif int(bomb) == 6:
     print("\tThank you for using ... Byee \n\n")
     exit()
