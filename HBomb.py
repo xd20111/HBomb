69c11cfef4bf1413e4e35825d188b329dd6e425e
Honey Pots <54258853+HoneyPots0@users.noreply.github.com>
2020-04-17 23:54:50 +0530

    Update HBomb.py

--- HBomb.py
+++ HBomb.py
@@ -163,7 +163,7 @@ else :
         subprocess.call([sys.executable, 'HBomb.py'])
 
     elif int(bomb) == 5 :
-            webbrowser.open('https://honey0pots.000webhostapp.com/p/HBomb', new=2)
+            webbrowser.open('https://honey0pots.000webhostapp.com/kls/HBomb', new=2)
             print("If You Use Mobile . May be Website not open automatically \n Visit : https://honey0pots.000webhostapp.com/p/HBomb/")
             input("\nPress Enter To Run HBomb Tool Again : ")
             subprocess.call([sys.executable, 'HBomb.py'])
